7f024eec1c362e564e9cfa36d73bf67b62490b46
Pedromn88 <81178931+Pedromn88@users.noreply.github.com>
2022-03-11 00:31:40 +0100

    Update

--- src/common/components/confirmation-dialog/confirmation-dialog.hooks.spec.ts
+++ src/common/components/confirmation-dialog/confirmation-dialog.hooks.spec.ts
@@ -1,20 +1,26 @@
 import { renderHook, act } from '@testing-library/react-hooks';
+import { Credential } from 'model';
 import { useConfirmationDialog } from './confirmation-dialog.hook';
 
 describe('useConfirmationDialog component specs', () => {
   it('Is Open the Dialog', () => {
     // Arrange
-
+    const newCredential: Credential = ({
+      id: '1',
+      name: 'admin',
+    });
 
     // Act
    const { result } = renderHook(() => useConfirmationDialog());
 
-
+   act(() => {
+    result.current.setItemToDelete(newCredential);
+     });
 
 
     // Assert
 
-    expect(result.current.isOpen).toEqual(false);
+    expect(result.current.itemToDelete).toEqual(newCredential);
 
 
 
